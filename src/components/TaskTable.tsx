d01002823da62d88e54872b4e9a2eea87c4fab0f
yu ping huang <xx4685441@gmail.com>
2024-04-13 19:34:34 +0800

    refactor: TaskTable component

--- src/components/TaskTable.tsx
+++ src/components/TaskTable.tsx
@@ -2,29 +2,17 @@ import { useEffect, useMemo, useRef } from "react";
 import { MRT_ColumnDef } from "material-react-table";
 import Table, { DataTableHandle } from "./Table";
 import Box from "@mui/material/Box/Box";
-
-const tasks = [
-  { id: 1, title: "task01", duration: 60, name: "Alice" }, // min
-  { id: 2, title: "task02", duration: 120, name: "Karen" },
-  { id: 3, title: "task03", duration: 180, name: "Miles" },
-  { id: 4, title: "task04", duration: 360, name: "Alice" },
-  { id: 5, title: "task05", duration: 30, name: "Bob" },
-  { id: 6, title: "task06", duration: 220, name: "Bob" },
-  { id: 7, title: "task07", duration: 640 },
-  { id: 8, title: "task08", duration: 250, name: "Karen" },
-  { id: 9, title: "task09", duration: 119, name: "Miles" },
-  { id: 10, title: "task10", duration: 560, name: "John" },
-  // { id: 11, title: "task11", duration: 340 },
-  // { id: 12, title: "task12", duration: 45 },
-  // { id: 13, title: "task13", duration: 86 },
-  // { id: 14, title: "task14", duration: 480 },
-  // { id: 15, title: "task15", duration: 900 },
-];
+import { tasks } from "../dummyData";
 
 type Task = {
   id: number;
   title: string;
   duration: number;
+  name: string;
+};
+
+type TaskArr = Task & {
+  index: number;
 };
 
 function TaskTable({ name }: { name: string | undefined }) {
@@ -54,11 +42,11 @@ function TaskTable({ name }: { name: string | undefined }) {
   useEffect(() => {
     if (name) {
       // eslint-disable-next-line prefer-const
-      let obj = {};
-      const taskArr: (Task & { index: number })[] = [];
+      let obj: { [x: number]: boolean } = {};
+      const taskArr: TaskArr[] = [];
       tasks.forEach((item, index) => {
         if (item.name === name) {
-          taskArr.push({ item, index });
+          taskArr.push({ ...item, index });
         }
       });
       taskArr.forEach((task) => {
@@ -71,7 +59,14 @@ function TaskTable({ name }: { name: string | undefined }) {
   }, [name]);
 
   return (
-    <Box sx={{ border: "1px solid #cccccc", background: "white", p: 2, borderRadius: 2 }}>
+    <Box
+      sx={{
+        border: "1px solid #cccccc",
+        background: "white",
+        p: 2,
+        borderRadius: 2,
+      }}
+    >
       <Box py={3} fontWeight="bolder" color="#838383">
         Tasks
       </Box>
--- src/dummyData.ts
+++ src/dummyData.ts
@@ -21,26 +21,16 @@ const employees = [
 ];
 
 const tasks = [
-  { id: 1, title: "task01", duration: 60 }, // min
-  { id: 2, title: "task02", duration: 120 },
-  { id: 3, title: "task03", duration: 180 },
-  { id: 4, title: "task04", duration: 360 },
-  { id: 5, title: "task05", duration: 30 },
-  { id: 6, title: "task06", duration: 220 },
+  { id: 1, title: "task01", duration: 60, name: "Alice" }, // min
+  { id: 2, title: "task02", duration: 120, name: "Karen" },
+  { id: 3, title: "task03", duration: 180, name: "Miles" },
+  { id: 4, title: "task04", duration: 360, name: "Alice" },
+  { id: 5, title: "task05", duration: 30, name: "Bob" },
+  { id: 6, title: "task06", duration: 220, name: "Bob" },
   { id: 7, title: "task07", duration: 640 },
-  { id: 8, title: "task08", duration: 250 },
-  { id: 9, title: "task09", duration: 119 },
-  { id: 10, title: "task10", duration: 560 },
-  { id: 11, title: "task11", duration: 340 },
-  { id: 12, title: "task12", duration: 45 },
-  { id: 13, title: "task13", duration: 86 },
-  { id: 14, title: "task14", duration: 480 },
-  { id: 15, title: "task15", duration: 900 },
+  { id: 8, title: "task08", duration: 250, name: "Karen" },
+  { id: 9, title: "task09", duration: 119, name: "Miles" },
+  { id: 10, title: "task10", duration: 560, name: "John" },
 ];
 
-export {
-  factories,
-  employeeType,
-  employees,
-  tasks
-}
+export { factories, employeeType, employees, tasks };
